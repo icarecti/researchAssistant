8315e960335a415913c2da31bd6bfa51f4656083
icarecti <icarecti@posteo.de>
2023-06-03 23:13:14 +0200

    create first simple analysis and write it to a markdown file

--- AnalysisService.py
+++ AnalysisService.py
@@ -1,24 +1,67 @@
+import datetime
+import os
+
+from dotenv import load_dotenv
+from langchain import ConversationChain
 from langchain.chat_models import ChatAnthropic
+from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
+
+load_dotenv()
+
+
+def generate_metadata(labels, score, url):
+    metadata = "---\n"
+    metadata += "tags: " + labels + "\n"
+    metadata += "score: " + score + "\n"
+    metadata += "date: " + str(datetime.date.today()) + "\n"
+    metadata += "source: " + url + "\n"
+    metadata += "status: created\n"
+    metadata += "---\n\n"
+    return metadata
 
 
 class AnalysisService:
     @staticmethod
     def analyse(data):
         url, text = data
-        print("################### before processing ###################")
-        print(text)
-
+        print("analyzing url: " + url)
         chat = ChatAnthropic()
         messages = [
             HumanMessage(
                 content="Write a summary of the [Text] that retains all important information while reducing the "
                         "word count to a minimum. Ensure that the summary accurately conveys the key points of the "
-                        "original text and that the meaning is not lost. Just return the summary:[Text]=" + text)
+                        "original text and that the meaning is not lost. Just return the summary in outline-form:"
+                        "[Text]=" + text)
         ]
-
+        print("calling claude for summary")
         response = chat(messages)
-        print("################### after processing ###################")
-        print(response)
+
+        llm = ChatOpenAI(temperature=0.9, model_name="gpt-4")
+        conversation = ConversationChain(llm=llm, verbose=False)
+        title_prompt = "summarize into one short title: " + response.content
+        labels_prompt = "Extract at least 10 labels of the [Text] that will be used categorize the text. Just return " \
+                        "the labels comma separated and ranked form most general to most specific. " \
+                        "[Text]=" + response.content
+        one_line_prompt = "summarize into one short line: " + response.content
+        score_prompt = "score the relevance of the following text in the field of large language models on a scale of " \
+                       "one to ten: " + response.content
+
+        print("calling gpt-4 for title, labels, one-liner and score based on summary")
+        title = conversation.predict(input=title_prompt)
+        labels = conversation.predict(input=labels_prompt)
+        one_liner = conversation.predict(input=one_line_prompt)
+        score = conversation.predict(input=score_prompt)
+
+        output_filename = os.getenv('OUTPUT_PATH') + title + '.md'
+
+        metadata = generate_metadata(labels, score, url)
+
+        print("writing to file: " + title)
+        with open(output_filename, 'w') as file:
+            file.write(metadata)
+            file.write(str(one_liner))
+            file.write("\n\n")
+            file.write(str(response.content))
 
         return url
--- ScrapingService.py
+++ ScrapingService.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 
+import tiktoken
 from bs4 import BeautifulSoup
 
 
@@ -11,9 +12,15 @@ class ScrapingService:
         # if image, then get a description
         # if pdf, then get the text
         # if blog, then get the text (with apify)
+        print("scraping url: " + url)
         page = urlopen(url)
         html = page.read().decode("utf-8")
         soup = BeautifulSoup(html, "html.parser")
         extracted_text = soup.getText().replace('\n', ' ')
         extracted_text = ' '.join(extracted_text.split())
+
+        encoding = tiktoken.encoding_for_model("gpt-3.5-turbo")
+        tokensize = len(encoding.encode(extracted_text))
+        print("extracted text with tokensize: " + str(tokensize))
+
         return url, extracted_text
