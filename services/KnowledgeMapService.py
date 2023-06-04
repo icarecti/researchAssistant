b3f5782fc526becd626a8a8f1efc07d85e3e14be
icarecti <icarecti@posteo.de>
2023-06-04 13:21:52 +0200

    change knowledgeMap to a grid

--- services/KnowledgeMapService.py
+++ services/KnowledgeMapService.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import uuid
 
@@ -10,14 +11,20 @@ class KnowledgeMapService:
     @staticmethod
     def create_map_from(notes):
         canvas = Canvas()
-        y_position = 0
-        for note in notes:
-            node = CanvasNode("file", note, 400, 400, uuid.uuid4().hex, 0, y_position)
+        node_width = 400
+        node_height = 400
+        column_spacing = 100
+        row_spacing = 100
+        columns = 4
+        for i, note in enumerate(notes):
+            x_position = (i % columns) * (node_width + column_spacing)
+            y_position = -(i // columns) * (node_height + row_spacing)
+            node = CanvasNode("file", note, node_width, node_height, uuid.uuid4().hex, x_position, y_position)
             canvas.add_node(node)
-            y_position -= 480
         canvas_dict = canvas.to_dict()
         canvas_text = json.dumps(canvas_dict)
 
-        output_filename = FilePathService.get_file_path("knowledge_map2.canvas")
+        map__canvas = "knowledge_map_" + str(datetime.date.today()) + ".canvas"
+        output_filename = FilePathService.get_file_path(map__canvas)
         with open(output_filename, 'w') as file:
             file.write(canvas_text)
